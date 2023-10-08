aaea4708b7154c58093f13521c4de181ebeed2cd
swapnil <91427197+swapnil-tayal@users.noreply.github.com>
2023-10-08 23:46:50 +0530

    finalizing

--- script/script.js
+++ script/script.js
@@ -11,31 +11,34 @@ let currentPage = 1;
 let searchWord = null;
 
 const downloadImg = (imgUrl) => {
-    
-    fetch(imgUrl).then(res => res.blob()).then(file => {
-        const a = document.createElement("a");
-        a.href = URL.createObjectURL(file);
-        a.download = new Date().getTime();
-        a.click();
-    }).catch(() => alert("~Failed to download image"));
-}
+  fetch(imgUrl)
+    .then((res) => res.blob())
+    .then((file) => {
+      const a = document.createElement("a");
+      a.href = URL.createObjectURL(file);
+      a.download = new Date().getTime();
+      a.click();
+    })
+    .catch(() => alert("~Failed to download image"));
+};
 
 const showLightbox = (name, img) => {
-    lightBox.querySelector("img").src = img;
-    lightBox.querySelector("span").innerHTML = name;
-    downloadImgBtn.setAttribute("data-img", img);
-    lightBox.classList.add("show");
-    document.body.style.overflow = "hidden";
-}
+  lightBox.querySelector("img").src = img;
+  lightBox.querySelector("span").innerHTML = name;
+  downloadImgBtn.setAttribute("data-img", img);
+  lightBox.classList.add("show");
+  document.body.style.overflow = "hidden";
+};
 
 const hideLightBox = () => {
-    lightBox.classList.remove("show");
-    document.body.style.overflow = "auto";
-}
+  lightBox.classList.remove("show");
+  document.body.style.overflow = "auto";
+};
 
 const generateHTML = (images) => {
-
-    imageWrapper.innerHTML += images.map(img => 
+  imageWrapper.innerHTML += images
+    .map(
+      (img) =>
         `<li class="card" onclick="showLightbox('${img.photographer}', '${img.src.large2x}')">
             <img src="${img.src.large2x}" alt="img">
             <div class="details">
@@ -47,47 +50,63 @@ const generateHTML = (images) => {
                     <i class="uil uil-import"></i>
                 </button>
             </div>
-        </li>`    
-    ).join("");
-
-}
+        </li>`
+    )
+    .join("");
+};
 
 const getImages = (apiURL) => {
+  loadMoreBtn.innerHTML = "Loading...";
+  loadMoreBtn.classList.add("disabled");
 
-    loadMoreBtn.innerHTML = "Loading...";
-    loadMoreBtn.classList.add("disabled");
-
-    fetch(apiURL, {
-        headers: {Authorization: apiKey}
-    }).then(res => res.json()).then(data => {
-        generateHTML(data.photos);
-        loadMoreBtn.innerHTML = "Load More";
-        loadMoreBtn.classList.remove("disabled");
-    }).catch(() => alert("Failed to load images"));
-
-}
+  fetch(apiURL, {
+    headers: { Authorization: apiKey },
+  })
+    .then((res) => res.json())
+    .then((data) => {
+      generateHTML(data.photos);
+      loadMoreBtn.innerHTML = "Load More";
+      loadMoreBtn.classList.remove("disabled");
+    })
+    .catch(() => alert("Failed to load images"));
+};
 
 const loadMoreImages = () => {
-    currentPage++;
-    let apiUrl = `https://api.pexels.com/v1/curated?page=${currentPage}&per_page=${perPage}`;
-    apiUrl = searchWord 
-             ? `https://api.pexels.com/v1/search?query=${searchWord}&page=${currentPage}&per_page=${perPage}`
-             : apiUrl;
-    getImages(apiUrl);
-}
+  currentPage++;
+  let apiUrl = `https://api.pexels.com/v1/curated?page=${currentPage}&per_page=${perPage}`;
+  apiUrl = searchWord
+    ? `https://api.pexels.com/v1/search?query=${searchWord}&page=${currentPage}&per_page=${perPage}`
+    : apiUrl;
+  getImages(apiUrl);
+};
 
 const loadSearchImages = (e) => {
-    if(e.target.value === "") return searchWord = null; 
-    if(e.key === "Enter"){
-        searchWord = e.target.value;
-        currentPage = 1;
-        imageWrapper.innerHTML = "";
-        getImages(`https://api.pexels.com/v1/search?query=${searchWord}&page=1&per_page=${perPage}`);
-    }
-}
+  if (e.target.value === "") return (searchWord = null);
+  if (e.key === "Enter") {
+    searchWord = e.target.value;
+    currentPage = 1;
+    imageWrapper.innerHTML = "";
+    getImages(
+      `https://api.pexels.com/v1/search?query=${searchWord}&page=1&per_page=${perPage}`
+    );
+  }
+};
 
-getImages(`https://api.pexels.com/v1/curated?page=${currentPage}&per_page=${perPage}`)
+getImages(
+  `https://api.pexels.com/v1/curated?page=${currentPage}&per_page=${perPage}`
+);
 loadMoreBtn.addEventListener("click", loadMoreImages);
 searchInput.addEventListener("keyup", loadSearchImages);
-closeBtn.addEventListener("click", hideLightBox);   
-downloadImgBtn.addEventListener("click", (e) => downloadImg(e.target.dataset.img));
+closeBtn.addEventListener("click", hideLightBox);
+downloadImgBtn.addEventListener("click", (e) =>
+  downloadImg(e.target.dataset.img)
+);
+
+// security
+document.addEventListener("contextmenu", (event) => event.preventDefault());
+document.onkeydown = function (e) {
+  if (event.keyCode == 123) return false;
+  if (e.ctrlKey && e.shiftKey && e.keyCode == "I".charCodeAt(0)) return false;
+  if (e.ctrlKey && e.shiftKey && e.keyCode == "J".charCodeAt(0)) return false;
+  if (e.ctrlKey && e.keyCode == "U".charCodeAt(0)) return false;
+};
