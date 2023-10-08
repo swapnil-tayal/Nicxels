29e8caee8c64154386f17a19f33da678d3b3185c
swapnil <91427197+swapnil-tayal@users.noreply.github.com>
2023-10-08 21:50:49 +0530

    final script1

--- script.js
+++ script.js
@@ -1,23 +1,24 @@
+const imageWrapper = document.querySelector(".images");
+const loadMoreBtn = document.querySelector(".load-more");
+const searchInput = document.querySelector(".search input");
+
 const apiKey = "d1Tqx2D3Q98YDEJwFIfmTIAidYzWdq3gdzW5jDWNqZgmBQCbXwXy5EmQ";
 const perPage = 15;
 let currentPage = 1;
-const imageWrapper = document.querySelector(".images");
-const loadMoreBtn = document.querySelector(".load-more");
-let searchInput = document.querySelector(".search-box input");
 let searchWord = null;
 
 const generateHTML = (images) => {
 
     imageWrapper.innerHTML += images.map(img => 
         `<li class="card">
-        <img src="${img.src.large2x}" alt="img">
-        <div class="details">
-            <div class="photographer">
-                <i class="uil uil-camera"></i>
-                <span>${img.photographer}</span>
+            <img src="${img.src.large2x}" alt="img">
+            <div class="details">
+                <div class="photographer">
+                    <i class="uil uil-camera"></i>
+                    <span>${img.photographer}</span>
+                </div>
+                <button><i class="uil uil-import"></i></button>
             </div>
-            <button><i class="uil uil-import"></i></button>
-        </div>
         </li>`    
     ).join("");
 
@@ -35,27 +36,26 @@ const getImages = (apiURL) => {
         loadMoreBtn.innerHTML = "Load More";
         loadMoreBtn.classList.remove("disabled");
     }).catch(() => alert("Failed to load images"));
-}
 
-console.log(searchInput);
+}
 
 const loadMoreImages = () => {
     currentPage++;
     let apiUrl = `https://api.pexels.com/v1/curated?page=${currentPage}&per_page=${perPage}`;
-    apiUrl = searchInput 
-             ? `https://api.pexels.com/v1/search?query=${searchInput}/page=${currentPage}&per_page=${perPage}`
+    apiUrl = searchWord 
+             ? `https://api.pexels.com/v1/search?query=${searchWord}&page=${currentPage}&per_page=${perPage}`
              : apiUrl;
     getImages(apiUrl);
 }
 
 const loadSearchImages = (e) => {
     if(e.target.value === "") return searchWord = null; 
-    if(e.key == "Enter"){
-        searchInput = e.target.value;
+    if(e.key === "Enter"){
+        searchWord = e.target.value;
+        console.log(searchWord);
         currentPage = 1;
         imageWrapper.innerHTML = "";
-        let apiUrl = `https://api.pexels.com/v1/search?query=${searchInput}/page=${currentPage}&per_page=${perPage}`;
-        getImages(apiUrl);
+        getImages(`https://api.pexels.com/v1/search?query=${searchWord}&page=1&per_page=${perPage}`);
     }
 }
 
